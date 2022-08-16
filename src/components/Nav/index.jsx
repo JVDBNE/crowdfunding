17f961aa726ba83d65fd52ca148d162076d0541c
Jess van Dyk <jessica.vandyk@xplortechnologies.com>
2022-08-16 20:35:43 +1000

    min css

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -6,6 +6,7 @@ const Nav = () => {
         <nav>
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
+            <Link to="/projects">New Project</Link>
         </nav>
     );
 }
